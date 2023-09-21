a6f9acfab0d2ca79ec4b656e1211350b7ad0cd8b
Miguel Romeral <miguel.romeral@gmail.com>
2023-09-21 18:46:20 +0200

    First components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { GenerateComponent } from './generate/generate.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'generate', component: GenerateComponent },
+  // Otras rutas pueden agregarse aquí
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+
