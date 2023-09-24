cbc3ae361ead9108ab29f4fd416d1cd3fd732ab6
Miguel Romeral <miguel.romeral@gmail.com>
2023-09-24 11:11:44 +0200

    Added some styles

--- src/app/components/screens/generate/generate.component.ts
+++ src/app/components/screens/generate/generate.component.ts
@@ -16,26 +16,80 @@ export class GenerateComponent implements OnInit{
 
   public racer: Racer = new Racer();
   
+  /* Driver */
+  public driverSkinIds: FormOption[] = FormOption.GetFromEnvironment(environment.driverSkinIds);
+  public driverSkinMaterialIds: FormOption[] = FormOption.GetFromEnvironment(environment.driverSkinMaterialIds);
+  public idsAnimations: FormOption[] = FormOption.GetFromEnvironment(environment.idsAnimations);
+  public celebrationAnimations: FormOption[] = FormOption.GetFromEnvironment(environment.celebrationAnimations);
+
+  /* Helmet */
+  public helmetIds: FormOption[] = FormOption.GetFromEnvironment(environment.helmetIds);
   public helmetMaterialsIds: FormOption[] = FormOption.GetFromEnvironment(environment.helmetMaterialsIds);
+  public visorIds: FormOption[] = FormOption.GetFromEnvironment(environment.visorIds);
+
+  /* Vehicle */
+  public vehicleMaterialIds: FormOption[] = FormOption.GetFromEnvironment(environment.vehicleMaterialIds);
 
   constructor(private jsonService: JsonConverterService) { }
 
   ngOnInit(): void {
   }
 
+  /* Driver */
+  racerDriverSkinEventListener(newValue: string) {
+    this.racer.driverSkin.id = newValue;
+    this.jsonService.updateRacer(this.racer);
+  }
+  racerDriverSkinMaterialEventListener(newValue: string) {
+    this.racer.driverSkin.materialId = newValue;
+    this.jsonService.updateRacer(this.racer);
+  }
+  racerAnimationEventListener(newValue: string) {
+    this.racer.idleAnimation = newValue;
+    this.jsonService.updateRacer(this.racer);
+  }
+  racerCelebrationAnimationsEventListener(newValue: string) {
+    this.racer.celebrationAnimation = newValue;
+    this.jsonService.updateRacer(this.racer);
+  }
 
   racerColor1EventListener(nuevaVariable: Color) {
     this.racer.color1 = nuevaVariable;
     this.jsonService.updateRacer(this.racer);
   }
+  racerColor2EventListener(nuevaVariable: Color) {
+    this.racer.color2 = nuevaVariable;
+    this.jsonService.updateRacer(this.racer);
+  }
+  racerColor3EventListener(nuevaVariable: Color) {
+    this.racer.color3 = nuevaVariable;
+    this.jsonService.updateRacer(this.racer);
+  }
+  racerColor4EventListener(nuevaVariable: Color) {
+    this.racer.color4 = nuevaVariable;
+    this.jsonService.updateRacer(this.racer);
+  }
+
+  /* Helmet */
   
+  racerHelmetIdEventListener(helmetId: string) {
+    this.racer.helmet.id = helmetId;
+    this.jsonService.updateRacer(this.racer);
+  }
+
   racerHelmetMaterialIdEventListener(helmetMaterialsId: string) {
     this.racer.helmet.materialId = helmetMaterialsId;
     this.jsonService.updateRacer(this.racer);
   }
 
-  updateValeListener(){
-
+  racerHelmetVisorEventListener(visorId: string) {
+    this.racer.helmet.visorId = visorId;
+    this.jsonService.updateRacer(this.racer);
   }
 
+  /* Vehicle */
+  racerVehicleMaterialEventListener(nuevaVariable: string) {
+    this.racer.vehicles[0].materialId = nuevaVariable;
+    this.jsonService.updateRacer(this.racer);
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,197 @@
 export const environment = {
+    /* Driver */
+    driverSkinIds: [
+        {
+            value: "driverskin-classic-m",
+            label: "Man"
+        },
+        {
+            value: "driverskin-classic-f",
+            label: "Woman"
+        },
+    ],
+    driverSkinMaterialIds: [
+        {
+            value: "driverskinmaterial-classic-m-0",
+            label: "Man"
+        },
+        {
+            value: "driverskinmaterial-classic-f-0",
+            label: "Woman"
+        },
+    ],
+    idsAnimations: [
+        {
+            value: "driveranimation-idle-01",
+            label: "Calm"
+        },
+        {
+            value: "driveranimation-idle-02",
+            label: "Ready"
+        },
+        {
+            value: "driveranimation-idle-03",
+            label: "Chill"
+        },
+        {
+            value: "driveranimation-idle-04",
+            label: "Alert"
+        },
+        {
+            value: "driveranimation-idle-05",
+            label: "Paparazzi"
+        },
+        {
+            value: "driveranimation-idle-07",
+            label: "Relaxed"
+        },
+        {
+            value: "driveranimation-idle-08",
+            label: "Twitch"
+        },
+        {
+            value: "driveranimation-idle-09",
+            label: "Sleepy"
+        },
+        {
+            value: "driveranimation-idle-10",
+            label: "Serious"
+        },
+        {
+            value: "driveranimation-idle-11",
+            label: "Comfy"
+        },
+        {
+            value: "driveranimation-idle-13",
+            label: "Challenger"
+        },
+        {
+            value: "driveranimation-idle-14",
+            label: "Shift"
+        },
+        {
+            value: "driveranimation-idle-15",
+            label: "Fashion"
+        },
+        {
+            value: "driveranimation-idle-16",
+            label: "Warm Up"
+        },
+        {
+            value: "driveranimation-idle-17",
+            label: "Daydreaming"
+        },
+        {
+            value: "driveranimation-idle-18",
+            label: "Pugilist"
+        },
+        {
+            value: "driveranimation-idle-19",
+            label: "Duelist"
+        },
+        {
+            value: "driveranimation-idle-20",
+            label: "Punch-Drunk"
+        },
+        {
+            value: "driveranimation-idle-21",
+            label: "Graceful Warrior"
+        },
+        {
+            value: "driveranimation-idle-22",
+            label: "Energy Master"
+        },
+        {
+            value: "driveranimation-idle-23",
+            label: "Light Fingers"
+        },
+        {
+            value: "driveranimation-idle-24",
+            label: "Painful Joints"
+        },
+        {
+            value: "driveranimation-idle-25",
+            label: "Salty"
+        },
+        {
+            value: "driveranimation-idle-27",
+            label: "Rumba"
+        },
+        {
+            value: "driveranimation-idle-28",
+            label: "Salsa"
+        },
+        {
+            value: "driveranimation-idle-29",
+            label: "Samba"
+        },
+        {
+            value: "driveranimation-idle-30",
+            label: "The Twist"
+        }
+    ],
+    celebrationAnimations: [
+        {
+            value: "driveranimation-celebration-01",
+            label: "Win"
+        },
+        {
+            value: "driveranimation-celebration-02",
+            label: "Joy"
+        },
+        {
+            value: "driveranimation-celebration-03",
+            label: "Jump"
+        },
+        {
+            value: "driveranimation-celebration-04",
+            label: "Wow!"
+        },
+        {
+            value: "driveranimation-celebration-05",
+            label: "Bow"
+        },
+        {
+            value: "driveranimation-celebration-06",
+            label: "Look!"
+        },
+        {
+            value: "driveranimation-celebration-07",
+            label: "Cheers"
+        },
+        {
+            value: "driveranimation-celebration-08",
+            label: "Style"
+        },
+        {
+            value: "driveranimation-celebration-09",
+            label: "Heart"
+        },
+        {
+            value: "driveranimation-celebration-10",
+            label: "Dance"
+        },
+        {
+            value: "driveranimation-celebration-12",
+            label: "Challenge"
+        },
+        {
+            value: "driveranimation-celebration-13",
+            label: "Formal"
+        },
+        {
+            value: "driveranimation-celebration-14",
+            label: "Gymnast"
+        }
+    ],
+    
+    /* Helmet */
+    helmetIds: [
+        {
+            value: "helmet-0-modern-full-face",
+            label: "Modern"
+        }
+    ],
     helmetMaterialsIds: [
         {
             value: "helmetmaterial-modern-full-face-0",
@@ -23,6 +216,112 @@ export const environment = {
         {
             value: "helmetmaterial-modern-full-face-kanaan",
             label: "Tony Kanaan"
+        }
+    ],
+    visorIds: [
+        {
+            value: "visor-acid-splash",
+            label: "Acid Splash"
+        },
+        {
+            value: "visor-black",
+            label: "Black"
+        },
+        {
+            value: "visor-clear-eyes-clear-skies",
+            label: "Clear Eyes Clear Skies"
+        },
+        {
+            value: "visor-cold-violet",
+            label: "Cold Violet"
+        },
+        {
+            value: "visor-cool-foof",
+            label: "Cool Foof"
+        },
+        {
+            value: "visor-filmic",
+            label: "Filmic"
+        },
+        {
+            value: "visor-gold",
+            label: "Gold"
+        },
+        {
+            value: "visor-hazardous-environments",
+            label: "Hazardous Environments"
+        },
+        {
+            value: "visor-lavender-days",
+            label: "Lavender Days"
+        },
+        {
+            value: "visor-piercing-red",
+            label: "Piercing Red"
+        },
+        {
+            value: "visor-royal",
+            label: "Royal"
+        },
+        {
+            value: "visor-salmon-dreams",
+            label: "Salmon Dreams"
+        },
+        {
+            value: "visor-shallow-seas",
+            label: "Shallow Seas"
+        },
+        {
+            value: "visor-silver",
+            label: "Silver"
+        },
+        {
+            value: "visor-sky",
+            label: "Sky"
+        },
+        {
+            value: "visor-spark",
+            label: "Spark"
+        },
+        {
+            value: "visor-sunset-1",
+            label: "Sunset 1"
+        },
+        {
+            value: "visor-sunset-2",
+            label: "Sunset 2"
+        },
+        {
+            value: "visor-sunset-3",
+            label: "Sunset 3"
+        },
+        {
+            value: "visor-two-tone",
+            label: "Two Tone"
+        },
+        {
+            value: "visor-wednesday",
+            label: "Wednesday"
+        },
+        {
+            value: "visor-witchcraft",
+            label: "Witchcraft"
+        }
+    ],
+
+    /* Vehicles */
+    vehicleMaterialIds: [
+        {
+            value: "vehiclematerial-standard-kart-0",
+            label: "OFK"
+        },
+        {
+            value: "vehiclematerial-standard-kart-1",
+            label: "Edge"
+        },
+        {
+            value: "vehiclematerial-standard-kart-2",
+            label: "Neotek"
         }
     ]
 };
